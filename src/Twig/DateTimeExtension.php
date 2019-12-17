3a4000f5be7df654bacde52d5251ae79c91d460c
Benjamin Kott <benjamin.kott@outlook.com>
2019-12-17 16:56:16 +0100

    [TASK] Add github-actions cleanup (#1)

--- src/Twig/DateTimeExtension.php
+++ src/Twig/DateTimeExtension.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package t3g/intercept.
+ * This file is part of the package t3g/symfony-template-bundle.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
