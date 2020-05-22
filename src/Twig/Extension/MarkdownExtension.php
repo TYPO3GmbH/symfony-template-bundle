9a1d6b1fbb71b6126b1cb84773b25676fef7078d
Benjamin Kott <benjamin.kott@outlook.com>
2020-05-22 10:13:47 +0200

    [TASK] Add markdown parser

---
+++ src/Twig/Extension/MarkdownExtension.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3g/symfony-template-bundle.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\Bundle\TemplateBundle\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class MarkdownExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('markdown', [$this, 'parse']),
+        ];
+    }
+
+    public function parse(string $markdown): string
+    {
+        $parsedown = new \Parsedown();
+        return $parsedown->parse($markdown);
+    }
+}
