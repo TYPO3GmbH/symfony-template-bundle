1063dbadd05ec9f103284b6804f3f1414acb736d
Andreas Fernandez <andreas.fernandez@typo3.com>
2020-09-16 11:46:46 +0200

    [BUGFIX] Don't treat choices.js as ES6 module
    The library choices.js was imported as a ES6 module which can fail as
    the module is no ES6 module and thus has no exports. The issue was that
    the code that would have been defined in exports is tried to get
    executed, which fails with an error, which never popped up as the error
    was not handled properly.
    
    To solve this issue, the module is now loaded via `require()` which
    solves the issue.
    
    Resolves: DEV-836

--- assets/js/form/choices.js
+++ assets/js/form/choices.js
@@ -1,11 +1,10 @@
 (function() {
     const elementsWithChoicesJs = document.querySelectorAll('[data-choicesjs]');
     if (elementsWithChoicesJs.length >= 1) {
-        import('choices.js').then(({ default: Choices }) => {
-            elementsWithChoicesJs.forEach(function (element) {
-                const options = element.dataset.choicesjs ? JSON.parse(element.dataset.choicesjs) : {};
-                new Choices(element, options);
-            });
-        }).catch(error => 'An error occurred while loading the choices component');
+        const Choices = require('choices.js');
+        elementsWithChoicesJs.forEach(function (element) {
+            const options = element.dataset.choicesjs ? JSON.parse(element.dataset.choicesjs) : {};
+            new Choices(element, options);
+        });
     }
 })();
