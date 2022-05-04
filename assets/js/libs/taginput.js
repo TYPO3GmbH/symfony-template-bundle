f8c1c9c7edb2f1e8575f6b32c5be9a3d13bffb35
Frank Naegler <frank.naegler@typo3.org>
2022-05-04 15:23:42 +0200

    [BUGFIX] Focus tagify field when original input gets the focus
    If the original input (which is hidden) get the focus. focus the tagify field instead.

--- assets/js/libs/taginput.js
+++ assets/js/libs/taginput.js
@@ -3,5 +3,9 @@ import Tagify from '@yaireo/tagify';
 var taginputElements = [].slice.call(document.querySelectorAll('[data-taginput]'));
 var taginputList = taginputElements.map(function (element) {
     const options = element.dataset.taginput ? JSON.parse(element.dataset.taginput) : {};
-    return new Tagify(element, options);
+    const tagify = new Tagify(element, options);
+    element.addEventListener('focus', function () {
+        tagify.DOM.input.focus();
+    });
+    return tagify;
 })
